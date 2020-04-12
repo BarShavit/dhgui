dba98c0c1c813aad43eeefa560a952b67e749328
Bar <barshavit230@gmail.com>
2020-04-12 21:47:30 +0300

    changed wl to behavior subject

--- dhgui/src/app/wl/components/wl/wl.component.ts
+++ dhgui/src/app/wl/components/wl/wl.component.ts
@@ -1,11 +1,12 @@
 import { WLChannel } from 'src/app/shared/models/wl/wl-channel';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import { WlService } from '../../services/wl.service';
 
 @Component({
   selector: 'app-wl',
   templateUrl: './wl.component.html',
-  styleUrls: ['./wl.component.scss']
+  styleUrls: ['./wl.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class WlComponent implements OnInit {
 
--- dhgui/src/app/wl/services/wl.service.ts
+++ dhgui/src/app/wl/services/wl.service.ts
@@ -2,17 +2,18 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { WLChannel } from 'src/app/shared/models/wl/wl-channel';
 import { ConstantsService } from 'src/app/shared/services/constants.service';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WlService {
 
-  channels: WLChannel[] = [];
+  channels$ = new BehaviorSubject<WLChannel[]>([]);
 
   constructor(private http: HttpClient, private constants: ConstantsService) {
-    this.http.get(this.constants.getWLChannels).toPromise().then(data =>{
-      this.channels = <WLChannel[]>data;
+    this.http.get(this.constants.getWLChannels).toPromise().then(data => {
+      this.channels$.next(<WLChannel[]>data);
     });
   }
 }
