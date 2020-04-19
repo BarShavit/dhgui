1c88e6c57094ef021f17d0ae95d87ac92f33aa9b
Bar <barshavit230@gmail.com>
2020-04-19 14:58:11 +0300

    when the screen is small and wan tabs get overflow, change it to select

--- dhgui/src/app/wan/components/wan/wan.component.ts
+++ dhgui/src/app/wan/components/wan/wan.component.ts
@@ -1,6 +1,7 @@
+import { BehaviorSubject } from 'rxjs';
 import { WanFullTopologyComponent } from './../wan-full-topology/wan-full-topology.component';
 import { WanService } from './../../services/wan.service';
-import { Component, OnInit, HostListener, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, HostListener, ChangeDetectionStrategy, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { MatDialog, MatDialogConfig } from '@angular/material/dialog';
 import { WarningComponent } from 'src/app/shared/components/warning/warning.component';
 import { TaskforceAddUpdateComponent } from '../taskforce-add-update/taskforce-add-update.component';
@@ -16,16 +17,22 @@ export enum KEY_CODE {
   styleUrls: ['./wan.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class WanComponent implements OnInit {
+export class WanComponent implements OnInit, AfterViewInit {
 
   selectedTab: number = 0;
   onOffMenuIcon: string = "";
   onOffMenuTitle: string = "";
+  tabsOverflow$ = new BehaviorSubject<boolean>(false);
+  @ViewChild("tabs", { read: ElementRef }) tabsElement: ElementRef | null = null;
 
   constructor(public wanService: WanService,
     private dialog: MatDialog) { }
 
   ngOnInit(): void {
+  }
+
+  ngAfterViewInit() {
+    this.tabsOverflow$.next(this.tabsOverflow());
   }
 
   getOnOffMenuIcon(): string {
@@ -91,6 +98,17 @@ export class WanComponent implements OnInit {
     return "red";
   }
 
+  private tabsOverflow(): boolean {
+    if (this.tabsElement == null || this.tabsElement.nativeElement == null) {
+      return false;
+    }
+
+    // Take the native element of the header only
+    // We can't do viewchild on inner angular material component without it
+    const nativeElement = this.tabsElement.nativeElement.childNodes[0];
+    return nativeElement.classList.length == 3;
+  }
+
   @HostListener('window:keyup', ['$event'])
   keyEvent(event: KeyboardEvent) {
     console.log(event);
@@ -102,5 +120,15 @@ export class WanComponent implements OnInit {
     if (event.keyCode === KEY_CODE.LEFT_ARROW) {
       this.selectedTab = 1;
     }
+  }
+
+  @HostListener('window:resize')
+  public detectResize(): void {
+    // This event fire before the gui rerender. So we give him 300ms
+    // and then check if the tabs overflow now
+    setTimeout(() => {
+      this.tabsOverflow$.next(this.tabsOverflow());
+    }, 300)
+    
   }
 }
